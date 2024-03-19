5256edfeabc47d5035e972d3e037b2eacee16ec1
mani agah esmaeilzad <74871543+mani-agah-esmaeilzad@users.noreply.github.com>
2024-03-19 09:21:20 +0330

    add package for convert datetime to persian and Gregorian

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eb7ec16e3482bdc5ef9876c1a0408ec5d223b862")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f083899b2eb55abad5313029cffb381cb3d32304")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
